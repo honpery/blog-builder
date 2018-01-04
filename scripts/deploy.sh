3b36c795ffe84a7ee3fe39ecd3a350a5bfd8c528
honpery <honpery@gmail.com>
2018-01-04 11:31:03 +0800

    add ci deploy scripts.

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd public
+git init 
+git config user.name honpery
+git config user.email honpery@gmail.com
+git add .
+git commit . -m "update by ci."
+git push -f "https://${GITHUB_TOKEN}@github.com/honpery/honpery.github.io.git" master:gh-pages
